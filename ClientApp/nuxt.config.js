b1221d662a58a6c88de9b1b0425f592217bcf725
@YousefNaddafi <yousefnaddafi@gmail.com>
2022-09-05 18:25:12 +0430

    asdf

--- ClientApp/nuxt.config.js
+++ ClientApp/nuxt.config.js
@@ -98,10 +98,10 @@ module.exports = {
       );
     },
   },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
+  //server: {
+  //  host: "0.0.0.0",
+  //  port: 3000,
+  //},
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     "@nuxtjs/recaptcha",
